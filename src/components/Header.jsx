73783578db004d8e19ce917cb3bab6db3219ae31
Benson Ngu <bensonngu25@gmail.com>
2024-12-04 19:10:17 +0800

    Configuration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 // src/components/Header.jsx
 import { useState } from 'react';
+import {Link} from 'react-router-dom';
 import { Dialog } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import LanguageSelector from './LanguageSelector';
@@ -17,14 +18,14 @@ export default function Header() {
     <header className="absolute inset-x-0 top-0 z-50 bg-transparent">
       <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
-          <a href="/" className="-m-1.5 p-1.5">
+          <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <img
               alt="Company Logo"
               src="./logo512.png"
               className="h-10 w-auto"
             />
-          </a>
+          </Link>
         </div>
         <div className="flex lg:hidden">
           <button
@@ -38,9 +39,9 @@ export default function Header() {
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
           {navigation.map((item) => (
-            <a key={item.name} href={item.href} className="text-xl font-semibold text-gray-900">
+            <Link key={item.name} href={item.href} className="text-xl font-semibold text-gray-900">
               {item.name}
-            </a>
+            </Link>
           ))}
         </div>
         <div class="hidden lg:flex lg:flex-1 lg:justify-end">
@@ -65,14 +66,14 @@ export default function Header() {
             }`}
         >
           <div className="flex items-center justify-between">
-            <a href="/" className="-m-1.5 p-1.5">
+            <Link href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img
                 alt="Company Logo"
                 src="./logo512.png"
                 className="h-10 w-auto"
               />
-            </a>
+            </Link>
             <button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
@@ -86,13 +87,13 @@ export default function Header() {
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
                 {navigation.map((item) => (
-                  <a
+                  <Link
                     key={item.name}
                     href={item.href}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold text-gray-900 hover:bg-gray-50"
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
               <div className='py-6'>
